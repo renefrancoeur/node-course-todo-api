e8a0f3c864c3dbdcb85923798d6eda1cd5836292
rene francoeur <renefrancoeur@yahoo.com>
2018-04-21 18:01:49 -0400

    get and post

--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ var app = express();
 
 app.use(bodyParser.json());
 
+
+//post route
 app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
@@ -21,6 +23,16 @@ app.post('/todos', (req, res) => {
   });
 
 });
+
+//get route
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos})
+  }, (e) => {
+    res.status(400).send(e);
+  })
+});
+
 
 app.listen(3000, () => {
   console.log('started on port 3000');
