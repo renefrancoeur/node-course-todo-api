4367571ae06772136b5a19ee1ea11093fc4e7d01
rene francoeur <renefrancoeur@yahoo.com>
2018-05-08 20:52:31 -0400

    add patch

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
+
 
 var {mongoose} = require('./db/mongoose.js'); //using ES6 get destructuring {} mongoose object from db/mongoose.js
 var {Todo} = require('./models/todo');
@@ -75,6 +77,33 @@ app.delete('/todos/:id', (req, res) => {
   }).catch((e) => {
     res.status(400).send();
   });
+});
+
+app.patch('/todos/:id', (req,res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();  //validate the ID - return a 404
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo)=> {
+      if(!todo) {
+        return res.status(404).send();
+      }
+
+      res.send({todo});
+    }).catch((e) => {
+      res.status(400).send();
+    })
 });
 
 
