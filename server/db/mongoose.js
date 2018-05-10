95aead05a8917575c80ff2f9a3f57a7d5cdfb544
rene francoeur <renefrancoeur@yahoo.com>
2018-05-10 18:24:11 -0400

    Setup seperate environemtn for test

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');//'protocol +url+ port and database'
+mongoose.connect(process.env.MONGODB_URI);//mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp') //set in enverionment under server.js other environment variables
+//'protocol +url+ port and database'
 
 module.exports = {mongoose};
