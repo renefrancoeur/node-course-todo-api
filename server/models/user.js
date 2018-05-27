d54330a9d11c98bc584bd69429b20d39dbb82c5b
rene francoeur <renefrancoeur@yahoo.com>
2018-05-27 19:14:21 -0400

    delete token private route

--- server/models/user.js
+++ server/models/user.js
@@ -53,6 +53,16 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+UserSchema.methods.removeToken = function (token) {
+  var user = this;
+  return user.update({
+                $pull: {
+                    tokens: {
+                      token: token
+                    }
+                }// $pull is a mongodb operator
+              });
+};
 
 UserSchema.statics.findByToken = function (token) {
   var User = this;
