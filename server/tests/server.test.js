914a0a6d2da730aa29ca17c14e7ac8d8c0a2de22
rene francoeur <renefrancoeur@yahoo.com>
2018-05-28 12:43:58 -0400

    test add for token removal on logout

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -276,7 +276,7 @@ describe('POST /users', () => {
 });
 
 
-describe('Post /users/login', () => {
+describe('POST /users/login', () => {
   it ('should login user and return auth token', (done) => {
     request(app)
     .post('/users/login')
@@ -327,6 +327,24 @@ describe('Post /users/login', () => {
 
     });
   });
+});
 
+describe('DELETE /users/me/token', () => {
+    it('should remove auth token on logout', (done) => {
+      request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .end((err,res) => {
+        if(err) {
+          return done(err);
+        }
 
+        User.findById(users[0]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch( (e) => done(e));
+
+      });
+    });
 });
